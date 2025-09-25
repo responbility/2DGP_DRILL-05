67278a4dc317b57640650b70cac5e2f292bda580
duruwell <saomaster4@gmail.com>
2025-09-25 22:21:49 +0900

    기초 배경 구현
    기초 배경 구현

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -1,21 +1,41 @@
 from pico2d import *
 
-
+open_canvas()
 TUK_WIDTH, TUK_HEIGHT = 1280, 1024
 backgrounds = load_image('TUK_GROUND.png')
+tuk_ground = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
 
+
+
+running = True
+frame = 0
+x, y = 400,90
+dir = 0
+move_dir = 0
+ANIM_ROW = 1
+
+
 def handle_events():
-    global running
-    global  x, y
+    global running ,dir, move_dir,ANIM_ROW
+    global x, y #전역좌표
+
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_MOUSEMOTION:
             x, y = event.x, TUK_HEIGHT - 1 - event.y
-        elif event.type == SDL_KEYDOWN and event.ky == SDLK_ESCAPE:
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 
 
-open_canvas()
+while running:
+    clear_canvas()
+    tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
+    character.clip_draw(frame * 100, 100  * 1 , 100, 100, x, y)
+
+    update_canvas()
+    handle_events()
+    frame = (frame + 1) % 8
+    delay(0.05)
