7070fe657a48934217eaa4ad9ccf3d953a473b12
duruwell <saomaster4@gmail.com>
2025-09-25 22:04:59 +0900

    change_file_2

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -1,33 +1,21 @@
 from pico2d import *
 
-open_canvas()
-grass = load_image('grass.png')
+
+TUK_WIDTH, TUK_HEIGHT = 1280, 1024
+backgrounds = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
 
-
-# fill here
-
-
-
 def handle_events():
-    # fill here
-    pass
+    global running
+    global  x, y
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_MOUSEMOTION:
+            x, y = event.x, TUK_HEIGHT - 1 - event.y
+        elif event.type == SDL_KEYDOWN and event.ky == SDLK_ESCAPE:
+            running = False
 
 
-frame = 0
-for x in range(0, 800, 5):
-
-    clear_canvas()
-    grass.draw(400, 30)
-    character.clip_draw(frame * 100, 100, 100, 100, x, 90)
-    update_canvas()
-
-    # fill here
-
-
-
-    frame = (frame + 1) % 8
-    delay(0.05)
-
-
-close_canvas()
+open_canvas()
